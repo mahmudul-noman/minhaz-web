7e30a3c4b2abb9dca832b932157fa9ba7350d673
mahmudul-noman <mhnoman75@gmail.com>
2025-11-13 12:45:52 +0600

    Update

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -8,15 +8,24 @@ const Hero = () => {
   return (
     <section id="hero" className="heroSection" aria-label="Introduction">
       <div className="heroContainer">
-        <p className="heroHello" aria-label="Greeting">Hello,</p>
-        <h1 className="heroTitle">
-          I'm <span className="heroName">Yoonus</span> Ajward
-        </h1>
-        <h2 className="heroSubtitle">Software Engineer</h2>
-        <p className="heroDescription">
-          I'm a passionate software engineer specializing in full-stack web and mobile application development. I enjoy crafting efficient, user-centric solutions that make a meaningful impact.
+        <p className="heroHello" aria-label="Greeting">
+          Hello,
         </p>
-        <div className="iconRow" role="navigation" aria-label="Social media links">
+        <h1 className="heroTitle">
+          I'm <span className="heroName">Minhaz</span> Uddin
+        </h1>
+        <h2 className="heroSubtitle">Backend Engineer (Python)</h2>
+        <p className="heroDescription">
+          I’m a backend software engineer specializing in Python and Django,
+          passionate about building scalable, secure, and high-performing
+          systems. I love turning ideas into reliable APIs and backend
+          architectures that empower seamless digital experiences.
+        </p>
+        <div
+          className="iconRow"
+          role="navigation"
+          aria-label="Social media links"
+        >
           <a
             href="https://www.linkedin.com/in/yoonus-ajward-461064239/"
             target="_blank"
@@ -47,7 +56,7 @@ const Hero = () => {
         </div>
         <div className="buttonRow">
           <a
-            href={`${process.env.PUBLIC_URL}/Yoonus_Associate_Software Engineer_CV.pdf`}
+            href={`${process.env.PUBLIC_URL}/Minhaz_Software Engineer_CV.pdf`}
             target="_blank"
             rel="noopener noreferrer"
             className="heroBtn"
@@ -55,14 +64,23 @@ const Hero = () => {
           >
             <FaDownload className="icon" /> Download Resume
           </a>
-          <Link to="contact" smooth duration={500} aria-label="Scroll to Contact section">
+          <Link
+            to="contact"
+            smooth
+            duration={500}
+            aria-label="Scroll to Contact section"
+          >
             <button className="heroBtn">
               <FaEnvelope className="icon" /> Contact Me
             </button>
           </Link>
         </div>
       </div>
-      <img src={heroimage} alt="Portrait of Yoonus Ajward" className="heroImage" />
+      <img
+        src={heroimage}
+        alt="Portrait of Yoonus Ajward"
+        className="heroImage"
+      />
     </section>
   );
 };
